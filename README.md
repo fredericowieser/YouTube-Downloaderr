075b62a1ca2af47bd0dd9b27fad700dd9c07d0f0
Frederico Wieser <20886677+fredericowieser@users.noreply.github.com>
2023-10-12 00:42:35 +0100

    Add converting from video to audio for cetain video types

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # YouTube Video Downloader
 
-Give a link along with what kind of video it is...
-    - song
-    - album
-    - lecture series
-    - one-off lecture
-    - podcast
-    - standard yt video
-    - dj set/mix
+**STATUS =** In Progress
 
+Give a link along with what kind of video it is...
+
+- song
+- album
+- lecture series
+- one-off lecture
+- podcast
+- standard yt video
+- dj set/mix
--- src/download.py
+++ src/download.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 import time
 import zipfile
 import shutil
@@ -9,13 +10,21 @@ from threading import current_thread
 from contextlib import contextmanager
 from pathlib import Path
 from io import StringIO
+from pprint import pprint
+
+
+#imageio.plugins.ffmpeg.download()
+# yt-dl: https://github.com/yt-dlp/yt-dlp
+ytdl = 'yt-dlp'
+ffmpeg_dir = r'/opt/homebrew/bin/ffmpeg'
+
 
 
 V_TYPES = (
     'Normal Vid',
-    'Song',
-    'Album (Playlist)',
-    'Album (1 Video)',
+    'Song (No Video)',
+    'Album (Playlist) (No Video)',
+    'Album (1 Video) (No Video)',
     'Lecture Series (Playlist)',
     'Single Lecture',
     'Podcast (No Video)',
@@ -26,21 +35,28 @@ V_TYPES = (
 )
 
 
+def convert_video_to_audio_ffmpeg(video_file, o_ext="mp3"):
+    """Converts video to audio directly using `ffmpeg` command
+    with the help of subprocess module"""
+    filename, ext = os.path.splitext(video_file)
+    os.system(
+        f"ffmpeg -y -i '{video_file}' '{filename}.{o_ext}'"
+    )
+    os.remove(f"{video_file}")
+
+
 @contextmanager
 def st_redirect(src, dst):
     placeholder = st.empty()
     output_func = getattr(placeholder, dst)
-
     with StringIO() as buffer:
         old_write = src.write
-
         def new_write(b):
             if getattr(current_thread(), SCRIPT_RUN_CONTEXT_ATTR_NAME, None):
                 buffer.write(b)
                 output_func(buffer.getvalue())
             else:
                 old_write(b)
-
         try:
             src.write = new_write
             yield
@@ -75,27 +91,30 @@ def set_directory(path: Path):
 
 def download(url, vtype):
     # Verify URL
-    if url.find('youtube') == -1: return 1 
+    if url.find('youtube') == -1: return Exception
 
     # Clean URL
     video_id = url.split('watch?v=')[1][:11]
     
+    audio = False
+    
     # Create Payload
-    # yt-dl: https://github.com/yt-dlp/yt-dlp
-    ytdl = 'yt-dlp'
-    cmd = f"{ytdl} --geo-bypass --no-check-certificate --write-description --write-link --ffmpeg-location='/opt/homebrew/bin/ffmpeg'"
+    cmd = f"{ytdl} --geo-bypass --no-check-certificate --write-description --write-link --ffmpeg-location {ffmpeg_dir}"
     if vtype == V_TYPES[0]:
         # Normal Video
         flags = '--all-subs --embed-thumbnail --no-playlist'
     elif vtype == V_TYPES[1]:
-        # Song
-        flags = '--extract-audio --metadata-from-title --no-playlist'
+        # Song (No Video)
+        flags = '--metadata-from-title --no-playlist'
+        audio = True
     elif vtype == V_TYPES[2]:
-        # Album (Playlist)
-        flags = '--extract-audio --metadata-from-title --yes-playlist'
+        # Album (Playlist) (No Video)
+        flags = '--metadata-from-title --yes-playlist'
+        audio = True
     elif vtype == V_TYPES[3]:
-        # Album (1 Video)
-        flags = '--extract-audio --metadata-from-title --no-playlist --write-comments --split-chapters'
+        # Album (1 Video) (No Video)
+        flags = '--metadata-from-title --no-playlist --write-comments --split-chapters'
+        audio = True
     elif vtype == V_TYPES[4]:
         # Lecture Series
         flags = '--embed-subs --embed-thumbnail --yes-playlist '
@@ -103,21 +122,24 @@ def download(url, vtype):
         # Single Lecture
         flags = '--embed-subs --write-auto-subs --embed-thumbnail --no-playlist'
     elif vtype == V_TYPES[6]:
-        # Podcast
+        # Podcast (No Video)
         flags = '--extract-audio --metadata-from-title --no-playlist'
+        audio = True
     elif vtype == V_TYPES[7]:
         # DJ Set
         flags = '--metadata-from-title --no-playlist --write-comments --split-chapters'
     elif vtype == V_TYPES[8]:
-        # Mix
-        flags = '--extract-audio --metadata-from-title --no-playlist --write-comments --split-chapters'
+        # Mix (No Video)
+        flags = '--metadata-from-title --no-playlist --write-comments --split-chapters'
+        audio = True
     elif vtype == V_TYPES[9]:
         # Short Film
         flags = '--all-subs --embed-thumbnail --no-playlist'
     elif vtype == V_TYPES[10]:
         # Playlist
         flags = '--all-subs --embed-thumbnail --yes-playlist'
-    
+    else:
+        return Exception
     
     payload = f'{cmd}{flags} {url}'
         
@@ -129,25 +151,46 @@ def download(url, vtype):
     if not os.path.exists(final_dir):
         os.makedirs(final_dir)
     
-    with set_directory(final_dir):
-        with st_stdout("code"):
-            os.system(payload)
-            
+    with st_stdout("code"):
+        with set_directory(final_dir):
+            stream = os.popen(payload)
+            pprint(stream.read())
+    
+    file_paths = [path for path in Path(final_dir).iterdir()]
+    
+    name = f"{file_paths[0].name.split('].')[0]}]"
+    
+    # Turn all Videos to Audio
+    if audio == True:
+        for file_path in file_paths:
+            if not file_path.name.find('.mp4') == -1:
+                with st_stdout("code"):
+                    with set_directory(final_dir):             
+                        convert_video_to_audio_ffmpeg(
+                            file_path.name,
+                            o_ext="mp3"
+                        )
+                        
+                        pprint("!!! VIDEO HAS CONVERTED TO AUDIO !!!")
+    
+    # Files have changed
+    file_paths = [path for path in Path(final_dir).iterdir()]
+    
     # Create Zip File
-    zip_name = f"{[path for path in Path(final_dir).iterdir()][0].name.split('].')}]"
-    with zipfile.ZipFile(f"{zip_name}.zip", mode="w") as archive:
-        for file_path in Path(final_dir).iterdir():
+    with zipfile.ZipFile(f"{name}.zip", mode="w") as archive:
+        for file_path in file_paths:
             archive.write(file_path, arcname=file_path.name)
     
     # Create Button To Download Zip File
-    with open(f"{zip_name}.zip", "rb") as file:
+    with open(f"{name}.zip", "rb") as file:
         btn = st.download_button(
             label="Download Compression",
             data=file,
-            file_name=f"{zip_name}.zip",
+            file_name=f"{name}.zip",
           )
     
     # Delete TMP directory
     shutil.rmtree(final_dir)
-    os.remove(f"{zip_name}.zip")
+    os.remove(f"{name}.zip")
+    
     
